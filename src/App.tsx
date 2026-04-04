81b546567bc923753f7ce5689a3127be2355c9c4
Damien Siri <dsiri12@gmail.com>
2026-04-03 22:11:13 -0400

    first submit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FaPencilAlt } from "react-icons/fa";
+
+const App = () => {
+  return ( <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
+    <h2 className="text-2xl font-bold mb-4 text-center"><button>
+      <FaPencilAlt />
+    </button> Notes App</h2>
+    </div> );
+}
+ 
+export default App;
