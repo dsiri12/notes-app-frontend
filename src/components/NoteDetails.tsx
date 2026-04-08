39e4e13e1e93d773a58d62be4f8743a7d041cecd
Damien Siri <dsiri12@gmail.com>
2026-04-07 21:12:10 -0400

    style the note

--- src/components/NoteDetails.tsx
+++ src/components/NoteDetails.tsx
@@ -6,22 +6,33 @@ type Props = {
 };
 
 const NoteDetails = ({ note, deleteNote }: Props) => {
+  const {id, title, priority, category, description} = note
+
+  let borderColor = ''
+  if (priority === 'High') {
+    borderColor = 'red'
+  } else if (priority === 'Medium') {
+    borderColor = 'orange'
+  } else {
+    borderColor = 'green'
+  }
+
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md border-l-4">
-      <h3 className="text-lg font-bold">{note.title}</h3>
+    <div className="p-4 bg-white rounded-lg shadow-md border-l-4" style={{borderLeftColor: borderColor}}>
+      <h3 className="text-lg font-bold">{title}</h3>
 
       <p className="text-sm text-gray-600">
-        <strong>Category: </strong> {note.category}
+        <strong>Category: </strong> {category}
       </p>
 
       <p className="text-sm text-gray-600">
-        <strong>Priority: </strong> {note.priority}
+        <strong>Priority: </strong> {priority}
       </p>
 
-      <p className="mt-2">{note.description}</p>
+      <p className="mt-2">{description}</p>
 
       <button
-        onClick={() => deleteNote(note.id)}
+        onClick={() => deleteNote(id)}
         className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700"
       >
         🗑️ Delete
