8a3aacf76682ee62283f2a610f39b8fe580fdcaf
Damien Siri <dsiri12@gmail.com>
2026-04-11 20:43:08 -0400

    add loading and error handling

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -2,11 +2,18 @@ import { useGetNotes } from "../hooks/useGetNotes";
 import NoteDetails from "./NoteDetails";
 
 const NoteList = () => {
-  const {data: notes} = useGetNotes()
+  const {data: notes, isLoading, isError} = useGetNotes()
 
-  if (!notes) { return }
+  if (isLoading) {
+    return <h3>Loading...</h3>
+  }
 
-  if (notes.length === 0) {
+  if (isError) {
+    return <h3>Something unexpected happened, retry later.</h3>
+  }
+
+  
+  if (!notes || notes.length === 0) {
     return <p className="text-center text-gray-500">No Notes Yet</p>;
   }
   
